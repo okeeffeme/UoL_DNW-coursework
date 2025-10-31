5f5f56af6802bff653dcef0277061a448936186e
thecodeduck <thecodeduck@gmail.com>
2025-10-31 19:09:07 +0100

    wk3_03

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
+const express = require("express"); 
 const http = require("http");
 const fs = require("fs");
 const path = require("path");
 
-const port = 8081;
+const port = 3000;
+const dt = require("./date.js");
 
 http.createServer(function(req, res) {
-    const fpath = path.join(__dirname, `index.html`);
-    fs.readFile(fpath, 'utf-8', (err, data) => {
-        if (err) {
-            res.writeHead(404, {'content-type': 'text/plain'});
-            res.end('404 Not Found');
-            return;
-        }
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.end(data);
-    })
+    // const fpath = path.join(__dirname, `index.html`);
+    // fs.readFile(fpath, 'utf-8', (err, data) => {
+    //     if (err) {
+    //         res.writeHead(404, {'content-type': 'text/plain'});
+    //         res.end('404 Not Found');
+    //         return;
+    //     }
+    //     res.writeHead(200, { "Content-Type": "text/html" });
+    //     res.end(data);
+    // })
+    res.writeHead(200, {"content-type": 'text/plain'});
+    res.end("The current date and time is: "+ dt.myDateTime() + "\n");
 }).listen(port, function() {
     console.log(`Node server is running on port ${port}...`);
 });
