5db546e2ce1093030b0e12d37bc6eb07e95773ed
thecodeduck <thecodeduck@gmail.com>
2025-11-02 16:33:54 +0100

    wk6_09

--- routes/main.js
+++ routes/main.js
@@ -4,11 +4,17 @@ const router = express.Router();
 
 // Handle the main routes
 router.get("/", (req, res) => {
-    res.render("index.ejs", {title: "Dynamic title"})
+    res.render("index.ejs", {title: "Hello"})
 });
 router.get("/about", (req, res) => {
-    res.render("about.ejs")
+    res.render("about.ejs", {title: "About"})
 });
-
+router.get("/search", (req, res) => {
+    res.render("search.ejs", {title: "Search"})
+});
+router.get("/search_result", function (req, res) {
+    // res.send("You searched for " + req.query.search_text);
+    res.render("results.ejs", {title: "Results", search_results: req.query.search_text})
+});
 // Export the router object so index.js can access it
 module.exports = router;
